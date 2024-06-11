5dab6fe3913f69cdcc58e969e907d4236493779c
Knowledgex187 <bizcuitx167@hotmail.co.uk>
2024-06-11 14:15:56 +0100

    All routes confirmed in views.py

--- website/views.py
+++ website/views.py
@@ -1,3 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
+
+
+# About Page
+@views.route("/")
+def home():
+    return render_template("about.html")
+
+
+# Bugatti Chiron Page
+@views.route("/bugatti-chiron")
+def bugatti_chiron():
+    return render_template("bugatti-chiron.html")
+
+
+# Bugatti Veyron Page
+@views.route("/bugatti-veyron")
+def bugatti_veyron():
+    return render_template("bugatti-veyron.html")
+
+
+# Conversions Page
+@views.route("/conversions")
+def conversions():
+    return render_template("conversions.html")
+
+
+# FAQ Page
+@views.route("/faq")
+def faq():
+    return render_template("faq.html")
+
+
+# History Page
+@views.route("/history")
+def history():
+    return render_template("history.html")
