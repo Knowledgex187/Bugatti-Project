2813351d8b0cfd36d3f0dc3272cee382808c38e5
Knowledgex187 <bizcuitx167@hotmail.co.uk>
2024-06-11 18:40:04 +0100

    Python code imputted and updated!

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 views = Blueprint("views", __name__)
 
@@ -6,34 +7,34 @@ views = Blueprint("views", __name__)
 # About Page
 @views.route("/")
 def home():
-    return render_template("about.html")
+    return render_template("about.html", user=current_user)
 
 
 # Bugatti Chiron Page
 @views.route("/bugatti-chiron")
 def bugatti_chiron():
-    return render_template("bugatti-chiron.html")
+    return render_template("bugatti-chiron.html", user=current_user)
 
 
 # Bugatti Veyron Page
 @views.route("/bugatti-veyron")
 def bugatti_veyron():
-    return render_template("bugatti-veyron.html")
+    return render_template("bugatti-veyron.html", user=current_user)
 
 
 # Conversions Page
 @views.route("/conversions")
 def conversions():
-    return render_template("conversions.html")
+    return render_template("conversions.html", user=current_user)
 
 
 # FAQ Page
 @views.route("/faq")
 def faq():
-    return render_template("faq.html")
+    return render_template("faq.html", user=current_user)
 
 
 # History Page
 @views.route("/history")
 def history():
-    return render_template("history.html")
+    return render_template("history.html", user=current_user)
